726eaa7533b8e36b9d059e99b487edaa01c038db
j.paul daigle <jdaigle1@student.gsu.edu>
2009-10-11 03:15:34 -0400

    works up to a point. Adds and subtracts nums, recognizes variables.

--- wae_test.rb
+++ wae_test.rb
@@ -3,50 +3,45 @@ $:.unshift File.join(File.dirname(__FILE__))
 require 'test/unit'
 require 'rubygems'
 require 'treetop'
+require 'polyglot'
 
 Treetop.load "waeg"
 
 
 class Wae_Grammar_Test < Test::Unit::TestCase
-
-
   def setup
     @parser = WaeParser.new
-  end #setup
+  end 
 
   def test_num
     assert(@parser.parse('0'))
-    assert(@parser.parse('90876'))
-    assert_equal 0, @parser.parse('0').eval
-    assert_equal 90210, @parser.parse('90210').eval
-    assert_not_equal 987, @parser.parse('234').eval
+    assert(@parser.parse('243090009234'))
+    assert_equal(5, @parser.parse('5').value)
   end
 
   def test_id
-    assert_equal 2, @parser.parse('x').eval({'x' => 2})
+    assert @parser.parse('d')
+    assert !@parser.parse('D')
+    assert !@parser.parse('xx')
+    assert_equal 'x', @parser.parse('x').name
   end
 
-  def test_negop
-    assert(@parser.parse('-x'))
-    assert_equal -5, @parser.parse('-5').eval
-    assert_equal -5, @parser.parse('-x').eval({'x' => 5})
+  def test_neg
+    assert(@parser.parse('-5'))
+    assert(@parser.parse('--x'))
+    assert_equal -3455, @parser.parse('-3455').value
   end
 
-  def test_sumop
-    assert(@parser.parse('{ + x {- 3 -4} }'))
-    assert_equal 7, @parser.parse('{ + 2 5 }').eval
-    assert_equal 11, @parser.parse('--{+{-3-4}{ +--5{+ 2 -3}}}').eval
-    assert_equal 4, @parser.parse('{ + x 2}').eval({'x' => 2})
-    assert_equal 2, @parser.parse('{+ 3 x}').eval({'x' => -1})
+  def test_addition
+    assert(@parser.parse('{- 6 5}'))
+    assert @parser.parse('{+6 5333 }')
+    assert_equal 7, @parser.parse('{+ 4 3}').value
+    assert_equal 28, @parser.parse('-{+ {- 32 -5} {+ -{+ 32 5} -28}}').value
   end
 
-  def test_with
-    assert(@parser.parse('{with {x 2} { + {with {y {with {z {+ 3 4}} z}}{+yz}} x}}'))
-    assert_equal 8, @parser.parse('{with { x 8 } x}').eval
-    assert_equal 14, @parser.parse('{+ {with { x 8 } x} 6}').eval
-    assert(@parser.parse('{+ {with { x 3 } x} {with {y 2} y}}'))
-    assert_equal 5, @parser.parse('{+ {with { x 3 } x} {with {y 2} y}}')
-#  assert_equal 6, @parser.parse('{with {x 2} { + x {with { y 4} y}}}').eval
+  def test_assignment
+    assert @parser.parse('{with {x 5} x}')
+    assert @parser.parse('{+ { with { x 5 } x} {with { y 4} y } }')
+    assert( @parser.parse('-{+ {with {x 5} x} {with {y 4} {+ x y}}}'))
   end
-
 end
