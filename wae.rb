e27e8ee4e3a19d9ca6a9a3a79d2650040f4c11ba
j.paul daigle <jdaigle1@student.gsu.edu>
2009-10-12 02:10:30 -0400

    Evaluates whether all these things parse or not.

--- wae.rb
+++ wae.rb
@@ -2,3 +2,24 @@ require 'rubygems'
 require 'treetop'
 
 Treetop.load "waeg"
+
+class Runtime
+  def initialize
+    @wae = WaeParser.new
+  end
+
+  def start
+    while true
+      print 'WAE > '
+      input = gets.chomp
+      if @wae.parse(input)
+        puts 'Valid WAE'
+      else
+        puts 'Invaled WAE'
+      end
+    end
+  end
+
+end
+
+Runtime.new.start
